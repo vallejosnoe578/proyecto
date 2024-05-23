bd87e55487267f063d399371b8e3567623b229a5
vallejos <vallejosnoe578@gmail.com>
2024-05-23 10:18:54 -0400

    se agrega el proyecto

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/tienda-youtube/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda-youtube";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "";
+
+const USER_SMTP = "Tu correo";
+const PASS_SMTP = "contraseña generada para esta aplicacion";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
---
+++ README.md
@@ -0,0 +1,14 @@
+# Tienda-online-php-mvc-y-mysql
+![tienda](https://github.com/VidaInformatico/Tienda-online-PHP-mvc-y-Mysql/assets/71534078/13df062f-c63d-4ca7-9437-d1bcc5acf4a9)
+
+## Configuración
+
+Instalación de Phpmailer
+
+```bash
+  composer install
+```
+    
+## Credenciales de acceso
+- Email: angelsifuentes2580@gmail.com
+- Clave: 12345
